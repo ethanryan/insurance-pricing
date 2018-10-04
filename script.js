f85a208574e4534302c2b37ef4a6ac1fcb0989af
ethanryan <ethanryan@gmail.com>
2018-10-03 23:34:17 -0400

    basic functionality working

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ allergies(boolean)
 sleep_apnea(boolean)
 heart_disease(boolean) */
 
+
+// var bob  = new User('bob');
+// console.log('bob.name is: ', bob.name)
+
 var form = document.getElementById("myForm");
 
 form.addEventListener("submit", function(evt) {
@@ -36,3 +40,97 @@ function getCheckedRadioValue(name) {
 /* function refreshPage(){
 window.location.reload();
 }  */
+
+
+//class
+
+class User {
+  constructor(name, age, gender, allergies, sleep_apnea, heart_disease) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.allergies = allergies;
+    this.sleep_apnea = sleep_apnea;
+    this.heart_disease = heart_disease;
+    // this.makeSound = () => `The ${this.type} named ${this.name} goes ${this.sound}!`;
+  }
+}
+
+// var em = new User('em', 23, 'male', 'false')
+// var bob = new User('bob', 29, 'male', 'true')
+// var susan = new User('susan', 87, 'female', 'false')
+//
+// console.log('em is: ', em)
+// console.log('bob is: ', bob)
+// console.log('susan is: ', susan)
+
+var kelly = new User('Kelly', 50, 'female', 'true', 'false', 'false')
+var josh = new User('Josh', 40, 'male', 'false', 'true', 'false')
+var brad = new User('Brad', 20, 'male', 'false', 'false', 'true')
+
+
+function getPrice(user) {
+  console.log('getPrice, user is: ', user)
+  const baseCost = 100
+  const baseAge = 18
+
+  let price = baseCost
+  console.log('price is now: ', price)
+
+  if (user.age < baseAge) {
+    return "ERROR! Must be over 18 to purchase this life insurance policy. Sorry!"
+  } else {
+    console.log(`${user.name}'s age is ${user.age}...`)
+    let ageRisk = user.age - baseAge //if 18, x is 1; if 19, x is 2...
+    // let agePremium = Math.ceil(ageRisk/5) * 5
+
+    // let agePremium = Math.floor(ageRisk/5) * 5
+    let agePremium = Math.floor(ageRisk/5) * 20
+
+
+    // let agePremium = 6 * 20 //NOTE: just for now!
+    // let agePremium = Math.floor(ageRisk/5)
+    // console.log('typeof agePremium is: ', typeof agePremium)
+    console.log('agePremium is: ', agePremium)
+    price = price + agePremium
+    console.warn('price is now: ', price)
+  }
+
+  if (user.allergies === 'true') {
+    price = price * (1 + 0.01)
+    console.warn('allergies - price is now: ', price)
+  }
+
+  if (user.sleep_apnea === 'true') {
+    price = price * (1 + 0.06)
+    console.warn('sleep_apnea - price is now: ', price)
+  }
+
+  if (user.heart_disease === 'true') {
+    price = price * (1 + 0.17)
+    console.warn('heart_disease - price is now: ', price)
+  }
+
+  if (user.gender === 'female') {
+    price = price - 12
+    console.warn('female discount - price is now: ', price)
+  }
+  console.log('final price is: ', price)
+  return price
+}
+
+// let emPrice = getPrice(em)
+// let bobPrice = getPrice(bob)
+// let susanPrice = getPrice(susan)
+//
+// console.log('emPrice is: ', emPrice)
+// console.log('bobPrice is: ', bobPrice)
+// console.log('susanPrice is: ', susanPrice)
+
+let kellyPrice = getPrice(kelly)
+let joshPrice = getPrice(josh)
+let bradPrice = getPrice(brad)
+
+console.log('kellyPrice is: ', kellyPrice)
+console.log('joshPrice is: ', joshPrice)
+console.log('bradPrice is: ', bradPrice)
